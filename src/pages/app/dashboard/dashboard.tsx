cf7e44dc240443fb78912e567dd6fc3a86b2b049
Rogerio-17 <rogeriojmf10@gmail.com>
2024-01-17 13:48:40 -0300

    Finalizando cards da pagina dashboard

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DayOrderAmountCard } from './day-order-amount-card'
+import { MonthCanceldOrderAmountCard } from './month-canceled-orders-amount-card'
+import { MonthOrderAmountCard } from './month-order-amount-card'
+import { MonthRevenueCard } from './month-revenue'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrderAmountCard />
+          <DayOrderAmountCard />
+          <MonthCanceldOrderAmountCard />
+        </div>
+      </div>
+    </>
+  )
+}
