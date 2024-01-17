00af18e59dcfbc72047455f74858be79e556a290
Rogerio-17 <rogeriojmf10@gmail.com>
2024-01-17 17:14:46 -0300

    Finalizando component de revenue-chart (grafico)

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrderAmountCard } from './day-order-amount-card'
 import { MonthCanceldOrderAmountCard } from './month-canceled-orders-amount-card'
 import { MonthOrderAmountCard } from './month-order-amount-card'
 import { MonthRevenueCard } from './month-revenue'
+import { RevenueChart } from './revenue-chart'
 
 export function Dashboard() {
   return (
@@ -17,6 +18,10 @@ export function Dashboard() {
           <MonthOrderAmountCard />
           <DayOrderAmountCard />
           <MonthCanceldOrderAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>
